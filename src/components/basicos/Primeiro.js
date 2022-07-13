f074603cae6d33924db7869788e54d3d197d0688
Alexsandro VcT <alexsandro.vct@outlook.com>
2022-07-13 11:27:27 -0300

    Atualizando

---
+++ src/components/basicos/Primeiro.js
@@ -0,0 +1,7 @@
+/**
+ * Primeiro componente, 
+Com relação ao padrão de nomenclaturas todos os componentes eu vou criar com o nome do arquivo com a primeira letra maiúscula.*/
+
+export default function Primeiro() {
+    return 'Primeiro Componente';
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
-import ReactDOM from 'react-dom';
+import './index.css' //Quero apenas que nossa aplicação seja capaz de encontra esse arquivo, todos os import que for fazer  relacionado a css vai ser dessa forma, referencia o arquivo
+import ReactDOM from 'react-dom'; // Posso coloca a variavel que eu quiser no ReactDOM
 import React from 'react';
 
+import Primeiro from './components/basicos/Primeiro'
 
-const tag = <strong>Olá React!</strong>
 
 ReactDOM.render(
     <div>
-        
-        { tag }
+
+    <Primeiro></Primeiro>
         
     </div>, //Sintaxi JSX
     document.getElementById('root')
